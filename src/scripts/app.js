a6b42e35dbd0c8bc33668ef020f6cd697c7fddc4
SmiKhyyyyz <119935653+SmiKhyyyyz@users.noreply.github.com>
2024-08-15 15:07:24 +0200

    Menu fin

--- src/scripts/app.js
+++ src/scripts/app.js
@@ -1,6 +1,8 @@
+"use strict";
 // MENU
 const burger = document.querySelector(".burger");
 burger.addEventListener('click', () => {
     const menu = document.querySelector(".menu");
     menu.classList.toggle("menu--active");
+    burger.classList.toggle("burger--active");
 })
